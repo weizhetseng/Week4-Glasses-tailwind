63c70b9f506ba825bd7d7762444d7dc57ce4c787
weizhetseng <weizhetseng@gmail.com>
2022-08-06 00:06:00 +0800

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '12px',
+    },
+    extend: {
+      colors: {
+        red: {
+          light: '#AA0601',
+          dark: '#650300',
+        }
+      },
+      backgroundImage: {
+        'banner-lg': "url('./assets/images/home-header.png')",
+        'banner-sm': "url('./assets/images/home-header-sm.png')",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
